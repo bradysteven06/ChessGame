075dfe2e3a3ce126ae401eb0db2c4cea06208950
Steven Brady <bradys@my.lanecc.edu>
2023-08-04 11:08:08 -0700

    added some more to the classes

--- Chess/Game/Board.cs
+++ Chess/Game/Board.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Chess.Pieces;
 
 namespace Chess.Game
 {
     internal class Board
     {
+        public Piece[][] Squares { get; set; }
     }
 }
--- Chess/Game/MoveValidator.cs
+++ Chess/Game/MoveValidator.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Chess.Pieces;
 
 namespace Chess.Game
 {
     internal class MoveValidator
     {
+        // Takes a piece and a target position then checks if the move is legal
+        private bool IsMoveLegal(Piece piece, Position destination)
+        {
+            // TODO: finish method
+            return false;
+        }
     }
 }
